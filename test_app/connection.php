13cbb725fe5897d0939e68484ae79c7f4b8abc4b
momone0124 <g026r034@gmail.com>
2025-09-10 12:10:29 +0900

    php_app作成

---
+++ test_app/connection.php
@@ -0,0 +1,60 @@
+<?php
+require_once('config.php');
+
+// PDOクラスのインスタンス化
+function connectPdo()
+{
+    try {
+        return new PDO(DSN, DB_USER, DB_PASSWORD);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit();
+    }
+}
+
+// 新規作成処理
+function createTodoData($todoText)
+{
+    $dbh = connectPdo();
+    $sql = 'INSERT INTO todos (content) VALUES ("' . $todoText . '")';
+    $dbh->query($sql);
+}
+
+// 更新処理
+function updateTodoData($post)
+{
+    $dbh = connectPdo();
+    $sql = 'UPDATE todos SET content = "' . $post['content'] . '" WHERE id = ' . $post['id'];
+    $dbh->query($sql);
+}
+
+
+function getTodoTextById($id)
+{
+    $dbh = connectPdo();
+    $sql = 'SELECT * FROM todos WHERE deleted_at IS NULL AND id =' . $id;
+    $data = $dbh->query($sql)->fetch();
+    return $data['content'];
+}
+
+function deleteTodoData($id)
+{
+    $dbh = connectPdo();
+    $now = date('Y-m-d H:i:s');
+
+     $sql = 'UPDATE todos SET deleted_at = :deleted_at WHERE id = :id';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute([
+        'deleted_at' => $now,
+        'id' => $id
+    ]);
+
+}
+
+function getAllRecords()
+{
+    $dbh = connectPdo();
+    $sql = 'SELECT * FROM todos WHERE deleted_at IS NULL';
+    return $dbh->query($sql)->fetchAll();
+}
+
